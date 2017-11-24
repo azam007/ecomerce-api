2edc618ea5f1e80d68d0899c1545ab44568944b9
Azam <rifqinewbie@gmail.com>
2017-11-24 14:24:57 +0700

    Modified Product Collection

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -5,13 +5,15 @@ namespace App\Http\Controllers;
 use App\Models\Product;
 use Illuminate\Http\Request;
 use App\Http\Resources\Product\ProductResource;
+use App\Http\Resources\Product\ProductCollection;
+
 
 class ProductController extends Controller
 {
 
     public function index()
     {
-        return Product::all();
+        return ProductCollection::collection(Product::all());
     }
 
 
