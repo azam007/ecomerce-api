770d6df7303aa74a0ac54cae00553e5a96da86c3
Azam <rifqinewbie@gmail.com>
2017-11-25 08:13:51 +0700

    Add TypeKey And Product Stored

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -6,20 +6,43 @@ use App\Models\Product;
 use Illuminate\Http\Request;
 use App\Http\Resources\Product\ProductResource;
 use App\Http\Resources\Product\ProductCollection;
-
+use App\Http\Requests\ProductRequest;
+use Symfony\Component\HttpFoundation\Response;
 
 class ProductController extends Controller
 {
 
+    public function __construct()
+    {
+        $this->middleware('auth:api')->except('index', 'show');
+    }
     public function index()
     {
         return ProductCollection::collection(Product::paginate(5));
     }
 
 
-    public function store(Request $request)
+    public function store(ProductRequest $request)
     {
-        //
+        $product = new Product([
+            'name' => $request->input('name'),
+            'detail' => $request->input('description'),
+            'stock' => $request->input('stock'),
+            'price' => $request->input('price'),
+            'discount' => $request->input('discount'),
+        ]);
+
+        $product->save();
+        return response()->json([
+            'success' => true,
+            'message' => 'Products Success Inserted!',
+            'data' => [
+                'type' => $product->typeKey(),
+                'id'   => $product->id,
+                'attributes' => new ProductResource($product)
+            ],
+
+        ], Response::HTTP_CREATED);
     }
 
 
--- app/Models/Product.php
+++ app/Models/Product.php
@@ -6,6 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
+
+    private $typeKey = 'products';
+    protected $fillable = [
+         'name', 'detail', 'price', 'stock', 'discount'
+    ];
+
+    public function typeKey()
+    {
+        return $this->typeKey;
+    }
     public function reviews()
     {
         return $this->hasMany(Review::class);
