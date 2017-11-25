d283a2daf80d97baa45c8f5d4df5a20e3269179a
Azam <rifqinewbie@gmail.com>
2017-11-25 09:54:09 +0700

    add Product Delete

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -74,6 +74,7 @@ class ProductController extends Controller
 
     public function destroy(Product $product)
     {
-        //
+        $product->delete();
+        return response()->json(null, Response::HTTP_NO_CONTENT);
     }
 }
